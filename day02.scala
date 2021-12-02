a15d518fad56e9c60d10b8668d74f9cc9ae96fa0
floh0 <florian.dasilva@ensiie.fr>
2021-12-02 18:51:05 +0100

    day 2

---
+++ day02.scala
@@ -0,0 +1,34 @@
+import scala.io.Source
+
+object Day1 {
+    val input: Seq[(String, Int)] = Source.fromFile("day02.input")
+        .getLines
+        .map(_.split(" "))
+        .map { case Array(i, v) => (i, v.toInt) }
+        .toSeq
+
+    def part1(): (Int, Int) = input
+        .foldLeft((0, 0)) {
+            case ((h, d), (i, v)) =>
+                if (i == "forward") (h+v, d)
+                else if (i == "down") (h, d+v)
+                else if (i == "up") (h, d-v)
+                else (h, d)
+        }
+
+    def part2(): (Int, Int, Int) = input
+        .foldLeft((0, 0, 0)) {
+            case ((a, h, d), (i, v)) =>
+                if (i == "forward") (a, h+v, d+v*a)
+                else if (i == "down") (a+v, h, d)
+                else if (i == "up") (a-v, h, d)
+                else (a, h, d)
+        }
+
+    def main(args: Array[String]): Unit = {
+        val (h1, d1) = part1()
+        println(s"$h1, $d1, ${h1*d1}") 
+        val (a2, h2, d2) = part2()
+        println(s"$a2, $h2, $d2, ${h2*d2}")
+    }
+}
